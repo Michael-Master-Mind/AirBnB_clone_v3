a2002f2785553b832c3f4d996763df38eec02eb0
wuhibe <wuhibetamire710@gmail.com>
2022-02-07 20:59:22 +0300

    import error

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,9 +4,8 @@ module to generate json response
 """
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, make_response, abort, request
 from models import storage
-from models.base_model import BaseModel, Base
 from models.state import State
 
 
@@ -29,14 +28,15 @@ def state_by_id(state_id):
     return jsonify(response.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def del_state(state_id=None):
     """ delete state by id """
     if state_id is None:
         abort(404)
     else:
         trash = storage.get(State, state_id)
-        if response is not None:
+        if trash is not None:
             storage.delete(trash)
             storage.save()
             return make_response(jsonify({}), 200)
